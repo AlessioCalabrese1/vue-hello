779512a0012a6b2a0b3122590d81fe626c970f4f
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-07-04 15:45:30 +0200

    Bonus

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = new Vue({
     el : "#root",
 
     data : {
-        message : "Titolone incredibile!"
+        message : "Titolone incredibile!",
+        imgSrc : "https://www.consumatori.it/wp-content/uploads/2018/10/Cani-spiaggia.png",
+        imgSrcButton : ""
+    },
+
+    methods : {
+        changeImg : function(){
+            this.imgSrc = this.imgSrcButton;
+        }
     }
 });
 
